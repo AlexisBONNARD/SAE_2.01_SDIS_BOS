287bc206254ed2b3b504e6e32e797dbec4489109
Luzagar <luzagar.abonner@gmail.com>
2024-06-04 11:47:02 +0200

    Add Crud

---
+++ SAE 2.01 SDIS BOS/Icrud.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_2._01_SDIS_BOS
+{
+    public interface Icrud
+    {
+
+        public int Create();
+        public int Delete();
+        public int Read();
+        public int ToString();
+        public int Update();
+    }
+}
