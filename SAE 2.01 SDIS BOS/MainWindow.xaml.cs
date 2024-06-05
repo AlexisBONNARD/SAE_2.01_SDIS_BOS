be7f4ecb9d8515b647cc83956812212cafddf3a8
Alexis Bonnard <alexis.bonnard@etu.univ-smb.fr>
2024-06-05 12:16:25 +0200

    nouveau disagne en raport a vecla demande de la prof V2

--- SAE 2.01 SDIS BOS/MainWindow.xaml.cs
+++ SAE 2.01 SDIS BOS/MainWindow.xaml.cs
@@ -42,43 +42,6 @@ namespace SAE_2._01_SDIS_BOS
         {
             InitializeComponent();
         }
-
-        private void Bold_Checked(object sender, RoutedEventArgs e)
-        {
-            textBox1.FontWeight = FontWeights.Bold;
-        }
-
-        private void Bold_Unchecked(object sender, RoutedEventArgs e)
-        {
-            textBox1.FontWeight = FontWeights.Normal;
-        }
-
-        private void Italic_Checked(object sender, RoutedEventArgs e)
-        {
-            textBox1.FontStyle = FontStyles.Italic;
-        }
-
-        private void Italic_Unchecked(object sender, RoutedEventArgs e)
-        {
-            textBox1.FontStyle = FontStyles.Normal;
-        }
-
-        private void IncreaseFont_Click(object sender, RoutedEventArgs e)
-        {
-            if (textBox1.FontSize < 18)
-            {
-                textBox1.FontSize += 2;
-            }
-        }
-
-        private void DecreaseFont_Click(object sender, RoutedEventArgs e)
-        {
-            if (textBox1.FontSize > 10)
-            {
-                textBox1.FontSize -= 2;
-            }
-        }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //this.Fenetre.FenetreAOuvrir = "Stop";
